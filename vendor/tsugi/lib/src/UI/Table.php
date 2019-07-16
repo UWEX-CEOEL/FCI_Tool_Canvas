7adb0557f822b8b1764c6db485ff9b4bf665bdfb
MarcellaThompson <31858759+MarcellaThompson@users.noreply.github.com>
2019-07-16 09:30:55 -0500

    Update Table.php

--- vendor/tsugi/lib/src/UI/Table.php
+++ vendor/tsugi/lib/src/UI/Table.php
@@ -23,7 +23,7 @@ use \Tsugi\Core\LTIX;
  */
 class Table {
 
-    public static $DEFAULT_PAGE_LENGTH = 200;  // Setting this to 2 is good for debugging
+    public static $DEFAULT_PAGE_LENGTH = 20;  // Setting this to 2 is good for debugging
 
     public static function doForm($values, $override=Array()) {
         foreach (array_merge($values,$override) as $key => $value) {
@@ -85,7 +85,8 @@ class Table {
         if ( $orderfields == false ) $orderfields = $searchfields;
 
         $searchtext = '';
-        if ( count($searchfields) > 0 && isset($params['search_text']) ) {
+        // if ( count($searchfields) > 0 && isset($params['search_text']) ) {
+        if ( isset($params['search_text']) ) {
             for($i=0; $i < count($searchfields); $i++ ) {
                 if ( $i > 0 ) $searchtext .= " OR ";
                 $searchtext .= $searchfields[$i]." LIKE :SEARCH".$i;
@@ -94,23 +95,11 @@ class Table {
         }
 
         $ordertext = '';
-        // if ( $params && isset($params['order_by']) && Table::matchColumns($params['order_by'], $orderfields) ) {
-        if ($params && isset($orderfields)) {
-            
-            $orderList = '';
-            
-            foreach ($orderfields as $orderItem) {
-                $orderList .= $orderItem;
-                
-                if ( isset($params['desc']) && $params['desc'] == 1) {
-                    $orderList .= " DESC";
-                }
-                
-                $orderList .= ", ";
+        if ( $params && isset($params['order_by']) && Table::matchColumns($params['order_by'], $orderfields) ) {
+            $ordertext = $params['order_by']." ";
+            if ( isset($params['desc']) && $params['desc'] == 1) {
+                $ordertext .= "DESC ";
             }
-            
-            $ordertext = substr_replace($orderList, "", -2);
-
         } else if ( count($orderfields) > 0 ) {
             $ordertext = $orderfields[0]." ";
         }
@@ -125,6 +114,7 @@ class Table {
             $desc = $params['desc']+0;
         }
 
+        $limittext = '';
         if ( $page_start < 1 ) {
             $limittext = "".($page_length+1);
         } else {
@@ -161,7 +151,6 @@ class Table {
         if ( strlen($limittext) > 0 ) {
             $newsql .= "\nLIMIT ".$limittext." ";
         }
-        
         return $newsql . "\n";
     }
 
@@ -276,7 +265,7 @@ class Table {
     <?php
     }
 
-    public static function pagedTable($rows, $searchfields=array(), $orderfields=false, $view=false, $params=false, $extra_buttons=false) 
+    public static function pagedTable($rows, $searchfields=array(), $orderfields=false, $view=false, $params=false, $extra_buttons=false)
     {
         if ( $params == false ) $params = $_GET;
         // Commenting out the header search and sort buttons.
@@ -287,7 +276,7 @@ class Table {
         $page_length = isset($params['page_length']) ? $params['page_length']+0 : self::$DEFAULT_PAGE_LENGTH;
         if ( $page_length < 0 ) $page_length = 0;
 
-        // When we are doing paging, we select page_length+1 rows to know whether 
+        // When we are doing paging, we select page_length+1 rows to know whether
         // we should show a Next button - but we don't want to show that extra
         // row in the output.
         if ( $page_length > 0 && $count > $page_length ) {
