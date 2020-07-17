bdbde658a4a91b904f77053a245b9c25bef256e4
MarcellaThompson <31858759+MarcellaThompson@users.noreply.github.com>
2020-07-17 10:09:33 -0500

    Update Index2.php

--- Index2.php
+++ Index2.php
@@ -1,3 +1,10 @@
-<html>
-<h1>Hello World</h1>
-</html>
+<?php
+?>
+<p>Thank you for your commitment to checking in for your course this month!  Unfortunately, you have accessed a retired version of the Flex Check-In.</p>
+ 
+<p>The current Flex Check-In questions are available in your course in the Flex Check-In module.  They are called Flex Check-In Question 1, Flex Check-In Question 2, and Flex Check-In Question 3.  Each of these items has a “rocket” icon beside them.</p> 
+ 
+<p>Please browse back to your course and click on the appropriate Flex Check-In questions.  If you have any trouble accessing your Flex Check-In you can be in touch with your Academic Success Coach or with tech support at <a href='https://ce.uwex.edu/technical-support/'>https://ce.uwex.edu/technical-support/</a>.</p>
+ 
+<?php
+?>
